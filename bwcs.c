28ae12369f9275192d8243c05368170a03d98655
flouMicaza <flo.mirandai@gmail.com>
2017-09-14 19:42:52 -0300

    Probandooolooo

--- bwcs.c
+++ bwcs.c
@@ -10,39 +10,50 @@
 #include "Data.h"
 #include <pthread.h>
 
-#define PORTUDP "2000"
-#define PORTtcp "2001"
+
 #define BUFFER_LENGTH 1400 //hay que definir que largo vamos a usarrrr
 
-int sudp; //socket udp
-int stcp; //socket stcp
 struct timeval t0, t1, t2;
-int t;
+int portTCP;
+char *portUDP="2001";
+int sudp;
+int stcp;
+int rdy=0;
+void* funcionTCP(void *puerto);
 
 
 
-//funcion que lee del tpc y lo escribe en udp
-void *funciontcp(){
-	int bytes, cnt;
-	char buffer[BUFFER_LENGTH];
-
-    gettimeofday(&t0, NULL);
-    for(bytes=0;; bytes+=cnt) {
-        cnt = Dread(stcp, buffer, BUFFER_LENGTH);
-		if(bytes == 0)
-			gettimeofday(&t1, NULL); /* Mejor esperar al primer read para empezar a contar */
-			write(sudp,NULL,0); //primer mensaje a udp
-		if(cnt <= 0)
-		    break;
-
-		//escribir en el servidor udp
-		write(sudp, buffer, cnt); //enviar a udp el mensaje que recibimos recien
-
-	}
+//funcion auxiliar que hace Dbind
+//creada para no enrtegar inputs en el thread
+void *DbindAux(){
+	Dbind(funcionTCP,"2000");
 	return NULL;
 }
 
-//funcion que recibe la respuesta del udp y se lo manda al connectstcp
+void *funcionTCP(void *puerto){
+	int bytes,cnt,packs;
+	char buffer[BUFFER_LENGTH];
+	gettimeofday(&t0,NULL);
+
+
+	portTCP=*((int*)puerto);
+	free(puerto);
+	fprintf(stderr, "cliente tcp conectado\n");
+
+	for(bytes=0,packs=0;; bytes+=cnt,packs++) {
+		cnt = Dread(portTCP, buffer, BUFFER_LENGTH);
+		if(bytes == 0)
+			gettimeofday(&t1, NULL); /* Mejor esperar al primer read para empezar a contar */
+			write(sudp,NULL,0); //primer mensaje a udp
+		if(cnt <= 0) 
+			break;
+		write(sudp, buffer, cnt);
+    }	
+
+	return NULL;
+}
+
+
 void *funcionUDP(){
 	int bytes, cnt;
 	char buffer[BUFFER_LENGTH];
@@ -53,52 +64,37 @@ void *funcionUDP(){
 	        
 	    }
 
-	    Dwrite(stcp, buffer, cnt); //devolvemos cosas a stcp
-	    Dwrite(stcp, buffer, 0); //avisa que termino
+	    Dwrite(portTCP, buffer, cnt); //devolvemos cosas a stcp
+	    
 	}
-
-	gettimeofday(&t2, NULL); //tiempo final
-
-    t = (t2.tv_sec*1.0+t2.tv_usec*1e-6) - (t1.tv_sec*1.0+t1.tv_usec*1e-6);
-    printf("El tiempo tardado es: %d\n", t);  
-    Dclose(stcp);
-    close(sudp);
-    return NULL;
+	//Dwrite(stcp, buffer, 0); //avisa que termino
+	rdy=1;
+	return NULL;
 }
 
-void Dbind_aux(){
-	Dbind(funciontcp, PORTtcp);
-}
 
 int main(){
-	//nose que se le pone a la parte de server
-	//talvez estas variables tengan que ser globales
-	
-	//conexion con el servidor stcp
-	/*stcp = Dconnect("localhost",PORTtcp);
-	if(sudp < 0) {
-	printf("connect stcp failed\n");
-       	exit(1);
-    }
-
-    printf("conectado\n");
-	*/
-	sudp = j_socket_udp_connect("localhost",PORTUDP);
+	sudp = j_socket_udp_connect("localhost","2001");
     if(sudp < 0) {
 	printf("connect UDP failed\n");
        	exit(1);
     }
 
-    printf("conectado\n");
-
+    printf(" UDP conectado\n");
     pthread_t pid1;
-
     pthread_t pid2;
-	pthread_create(&pid1,NULL,Dbind,funciontcp,2000);
+
+	pthread_create(&pid1,NULL,DbindAux,NULL);
 	pthread_create(&pid2,NULL,funcionUDP,NULL);
 
-	//falta enterrar a los thread y cachar cuando hay q enterrarlos
+	while(!rdy){
+		;
+	}
 
+	pthread_join(pid1,NULL);
+	pthread_join(pid2,NULL);
+	Dclose(stcp);
+    close(sudp);
 	return 0;
-}
 
+}
